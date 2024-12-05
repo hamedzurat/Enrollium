9da1271a6d50bd6a99427ae380fc590c679145ec
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-12-05 23:52:30 +0600

    Update Main.java

--- client/src/main/java/enrollium/client/Main.java
+++ client/src/main/java/enrollium/client/Main.java
@@ -4,21 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/enrollium/client/counter.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/enrollium/client/MainApp.fxml"));
         Scene      scene      = new Scene(fxmlLoader.load());
 
         stage.setTitle("Enrollium");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
