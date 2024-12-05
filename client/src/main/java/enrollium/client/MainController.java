c5422723fccab392d00b714a959158990cca5042
Adham Zarif <zarifseam52@gmail.com>
2024-12-05 23:50:09 +0600

    Updated Client Files

--- client/src/main/java/enrollium/client/MainController.java
+++ client/src/main/java/enrollium/client/MainController.java
@@ -1,57 +1,3 @@
-//package enrollium.client;
-//
-//import io.reactivex.rxjava3.core.Observable;
-//import io.reactivex.rxjava3.disposables.CompositeDisposable;
-//import io.reactivex.rxjava3.subjects.PublishSubject;
-//import javafx.application.Platform;
-//import javafx.fxml.FXML;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.Label;
-//
-//
-//public class MainController {
-//    private final PublishSubject<Integer> clickSubject = PublishSubject.create();
-//    private final CompositeDisposable     disposables  = new CompositeDisposable();
-//    @FXML
-//    private       Label                   counterLabel;
-//    @FXML
-//    private       Button                  incrementButton;
-//
-//    @FXML
-//    public void initialize() {
-//        // Set up the counter observable
-//        Observable<Integer> counterObservable = clickSubject.scan(0, (count, value) -> count + 1)
-//                                                            .distinctUntilChanged() // only change if new
-//                                                            .doOnError(error -> Platform.runLater(() -> {
-//                                                                counterLabel.setText("Error");
-//                                                            }))
-//                                                            .retry() // resubscribes after errors
-//                                                            .share(); // ot and sharable observable
-//
-//        // Subscribe to counter updates
-//        disposables.add( // for cleaning subscriptions to prevent memory
-//                counterObservable.subscribe( // say what will happen when THE button is clicked.
-//                        count -> Platform.runLater(() -> {
-//                            counterLabel.setText(String.valueOf(count));
-//                            System.out.println(count);
-//                        }), //
-//                        error -> System.err.println("Error in counter stream: " + error) //
-//                ));
-//
-//        // Connect button clicks to the subject
-//        incrementButton.setOnAction(event -> clickSubject.onNext(1)); // trigger
-//
-//        // Set up cleanup when window closes
-//        Platform.runLater(() -> {
-//            incrementButton.getScene().getWindow().setOnCloseRequest(event -> {
-//                if (!disposables.isDisposed()) {
-//                    disposables.dispose();
-//                }
-//            });
-//        });
-//    }
-//}
-
 package enrollium.client;
 
 import javafx.fxml.FXML;
@@ -61,42 +7,41 @@ import javafx.scene.text.Text;
 
 import java.util.HashMap;
 
+
 public class MainController {
-
-    @FXML
-    public TabPane tabPane;
-    @FXML
-    public GridPane planner;
-    @FXML
-    public ComboBox<String> courseDropdown;
-    @FXML
-    public Button takeCourseButton;
-    @FXML
-    public Text courseFeedback;
-    @FXML
-    public ListView<String> sections;
-    @FXML
-    public Button tradeButton;
-    @FXML
-    public Text tradeFeedback;
-    @FXML
-    public ListView<String> notifications;
-    @FXML
-    public Text badgeCounter;
-    @FXML
-    public ListView<String> groupMembers;
-    @FXML
-    public Button compareSchedulesButton;
-    @FXML
-    public Text scheduleFeedback;
-
     public final HashMap<String, String> courseSchedule = new HashMap<>();
+    @FXML
+    public       TabPane                 tabPane;
+    @FXML
+    public       GridPane                planner;
+    @FXML
+    public       ComboBox<String>        courseDropdown;
+    @FXML
+    public       Button                  takeCourseButton;
+    @FXML
+    public       Text                    courseFeedback;
+    @FXML
+    public       ListView<String>        sections;
+    @FXML
+    public       Button                  tradeButton;
+    @FXML
+    public       Text                    tradeFeedback;
+    @FXML
+    public       ListView<String>        notifications;
+    @FXML
+    public       Text                    badgeCounter;
+    @FXML
+    public       ListView<String>        groupMembers;
+    @FXML
+    public       Button                  compareSchedulesButton;
+    @FXML
+    public       Text                    scheduleFeedback;
 
     @FXML
     public void initialize() {
         populateCourseSchedule();
 
-        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        String[] days      = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
         String[] timeSlots = {"8-9 AM", "9-10 AM", "10-11 AM", "11-12 PM", "12-1 PM", "1-2 PM"};
 
         for (int i = 0; i < days.length; i++) {
@@ -115,7 +60,7 @@ public class MainController {
                 {1, 6}, {2, 6}, {3, 6}, {4, 6}, {5, 6}
         };
 
-        int courseIndex = 0;
+        int      courseIndex = 0;
         String[] courseCodes = courseSchedule.keySet().toArray(new String[0]);
         for (int[] position : coursePositions) {
             if (courseIndex >= courseCodes.length) break;
