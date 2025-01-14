541307c2e3512a59966641f2181136179ed39656
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2025-01-15 05:17:50 +0600

    Section Files Added

---
+++ client/src/main/java/enrollium/client/page/general/SpaceTimeFormView.java
@@ -0,0 +1,83 @@
+package enrollium.client.page.general;
+
+import enrollium.client.page.OutlinePage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class SpaceTimeFormView extends OutlinePage {
+
+    public static final String NAME = "SpaceTime Form View";
+
+    public SpaceTimeFormView() {
+        super();
+
+        // Add page header
+        addPageHeader();
+
+        // Add a section for the form
+        addSection("SpaceTime Form", createFormView());
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    private VBox createFormView() {
+        VBox root = new VBox(15);
+        root.setPadding(new Insets(20));
+        root.setAlignment(Pos.TOP_LEFT);
+
+        // Create form fields
+        GridPane form = new GridPane();
+        form.setHgap(10);
+        form.setVgap(10);
+        form.setPadding(new Insets(10));
+
+        Label roomNameLabel = new Label("Room Name:");
+        TextField roomNameField = new TextField();
+
+        Label roomNumberLabel = new Label("Room Number:");
+        TextField roomNumberField = new TextField();
+
+        Label roomTypeLabel = new Label("Room Type:");
+        TextField roomTypeField = new TextField();
+
+        Label dayLabel = new Label("Day:");
+        TextField dayField = new TextField();
+
+        Label timeSlotLabel = new Label("Time Slot:");
+        TextField timeSlotField = new TextField();
+
+        Button submitButton = new Button("Submit");
+        submitButton.setOnAction(event -> {
+            // Logic to handle form submission
+            System.out.println("Room Name: " + roomNameField.getText());
+            System.out.println("Room Number: " + roomNumberField.getText());
+            System.out.println("Room Type: " + roomTypeField.getText());
+            System.out.println("Day: " + dayField.getText());
+            System.out.println("Time Slot: " + timeSlotField.getText());
+        });
+
+        // Add form fields to grid
+        form.add(roomNameLabel, 0, 0);
+        form.add(roomNameField, 1, 0);
+        form.add(roomNumberLabel, 0, 1);
+        form.add(roomNumberField, 1, 1);
+        form.add(roomTypeLabel, 0, 2);
+        form.add(roomTypeField, 1, 2);
+        form.add(dayLabel, 0, 3);
+        form.add(dayField, 1, 3);
+        form.add(timeSlotLabel, 0, 4);
+        form.add(timeSlotField, 1, 4);
+
+        root.getChildren().addAll(form, submitButton);
+
+        return root;
+    }
+}
---
+++ client/src/main/java/enrollium/client/page/general/SpaceTimeTableView.java
@@ -0,0 +1,133 @@
+package enrollium.client.page.general;
+
+import enrollium.client.page.OutlinePage;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+
+public class SpaceTimeTableView extends OutlinePage {
+
+    public static final String NAME = "SpaceTime Table View";
+    private final ObservableList<SpaceTime> spaceTimeData;
+
+    // Default constructor with some initial content
+    public SpaceTimeTableView() {
+        this(FXCollections.observableArrayList(
+                new SpaceTime("Room 101", "101", "Theory", "Monday", "08:00 - 09:30"),
+                new SpaceTime("Room 102", "102", "Lab", "Tuesday", "10:00 - 11:30"),
+                new SpaceTime("Room 201", "201", "Seminar", "Wednesday", "13:00 - 14:30"),
+                new SpaceTime("Room 301", "301", "Conference", "Thursday", "15:00 - 16:30"),
+                new SpaceTime("Room 401", "401", "Theory", "Friday", "17:00 - 18:30")
+        ));
+    }
+
+    // Constructor with ObservableList parameter
+    public SpaceTimeTableView(ObservableList<SpaceTime> spaceTimeData) {
+        super();
+        this.spaceTimeData = spaceTimeData;
+
+        // Add page header
+        addPageHeader();
+
+        // Add a section for the table
+        addSection("SpaceTime Table", createTableView());
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    private VBox createTableView() {
+        VBox root = new VBox(10);
+
+        TableView<SpaceTime> table = new TableView<>();
+
+        // Define Columns
+        TableColumn<SpaceTime, String> roomNameColumn = new TableColumn<>("Room Name");
+        roomNameColumn.setCellValueFactory(data -> data.getValue().roomNameProperty());
+
+        TableColumn<SpaceTime, String> roomNumberColumn = new TableColumn<>("Room Number");
+        roomNumberColumn.setCellValueFactory(data -> data.getValue().roomNumberProperty());
+
+        TableColumn<SpaceTime, String> roomTypeColumn = new TableColumn<>("Room Type");
+        roomTypeColumn.setCellValueFactory(data -> data.getValue().roomTypeProperty());
+
+        TableColumn<SpaceTime, String> dayColumn = new TableColumn<>("Day");
+        dayColumn.setCellValueFactory(data -> data.getValue().dayProperty());
+
+        TableColumn<SpaceTime, String> timeSlotColumn = new TableColumn<>("Time Slot");
+        timeSlotColumn.setCellValueFactory(data -> data.getValue().timeSlotProperty());
+
+        // Add columns to table
+        table.getColumns().addAll(roomNameColumn, roomNumberColumn, roomTypeColumn, dayColumn, timeSlotColumn);
+
+        // Bind TableView to shared ObservableList
+        table.setItems(spaceTimeData);
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        root.getChildren().addAll(new Label("SpaceTime Slots Overview"), table);
+        return root;
+    }
+
+    // Mock SpaceTime class
+    public static class SpaceTime {
+        private final SimpleStringProperty roomName;
+        private final SimpleStringProperty roomNumber;
+        private final SimpleStringProperty roomType;
+        private final SimpleStringProperty day;
+        private final SimpleStringProperty timeSlot;
+
+        public SpaceTime(String roomName, String roomNumber, String roomType, String day, String timeSlot) {
+            this.roomName = new SimpleStringProperty(roomName);
+            this.roomNumber = new SimpleStringProperty(roomNumber);
+            this.roomType = new SimpleStringProperty(roomType);
+            this.day = new SimpleStringProperty(day);
+            this.timeSlot = new SimpleStringProperty(timeSlot);
+        }
+
+        public String getRoomName() {
+            return roomName.get();
+        }
+
+        public SimpleStringProperty roomNameProperty() {
+            return roomName;
+        }
+
+        public String getRoomNumber() {
+            return roomNumber.get();
+        }
+
+        public SimpleStringProperty roomNumberProperty() {
+            return roomNumber;
+        }
+
+        public String getRoomType() {
+            return roomType.get();
+        }
+
+        public SimpleStringProperty roomTypeProperty() {
+            return roomType;
+        }
+
+        public String getDay() {
+            return day.get();
+        }
+
+        public SimpleStringProperty dayProperty() {
+            return day;
+        }
+
+        public String getTimeSlot() {
+            return timeSlot.get();
+        }
+
+        public SimpleStringProperty timeSlotProperty() {
+            return timeSlot;
+        }
+    }
+}
