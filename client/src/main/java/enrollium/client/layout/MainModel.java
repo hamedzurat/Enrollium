67a88c3c9878c3e8b810480fda141aadc27c8406
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2025-01-15 05:18:46 +0600

    Updated MainModel.java

--- client/src/main/java/enrollium/client/layout/MainModel.java
+++ client/src/main/java/enrollium/client/layout/MainModel.java
@@ -4,7 +4,7 @@ import enrollium.client.event.DefaultEventBus;
 import enrollium.client.event.NavEvent;
 import enrollium.client.page.Page;
 import enrollium.client.page.debug.Button;
-import enrollium.client.page.general.ThemePage;
+import enrollium.client.page.general.*;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import org.kordamp.ikonli.javafx.FontIcon;
@@ -15,12 +15,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-
 public class MainModel {
-    public static final  Class<? extends Page>                        DEFAULT_PAGE = Button.class;
-    private static final Map<Class<? extends Page>, NavTree.Item>     NAV_TREE     = createNavItems();
-    private final        ReadOnlyObjectWrapper<Class<? extends Page>> selectedPage = new ReadOnlyObjectWrapper<>();
-    private final        ReadOnlyObjectWrapper<NavTree.Item>          navTree      = new ReadOnlyObjectWrapper<>(createTree());
+    public static final Class<? extends Page> DEFAULT_PAGE = Button.class;
+    private static final Map<Class<? extends Page>, NavTree.Item> NAV_TREE = createNavItems();
+    private final ReadOnlyObjectWrapper<Class<? extends Page>> selectedPage = new ReadOnlyObjectWrapper<>();
+    private final ReadOnlyObjectWrapper<NavTree.Item> navTree = new ReadOnlyObjectWrapper<>(createTree());
 
     public MainModel() {
         // Load page if NavEvent happens
@@ -35,7 +34,12 @@ public class MainModel {
         map.put(Button.class, NavTree.Item.page(Button.NAME, Button.class));
 
         // general
-        map.put(ThemePage.class, NavTree.Item.page(ThemePage.NAME, ThemePage.class));
+
+        // new pages
+        map.put(SpaceTimeFormView.class, NavTree.Item.page("SpaceTime Form", SpaceTimeFormView.class));
+        map.put(SpaceTimeTableView.class, NavTree.Item.page("SpaceTime Table", SpaceTimeTableView.class));
+        map.put(SectionFormView.class, NavTree.Item.page("Section Form", SectionFormView.class));
+        map.put(SectionTableView.class, NavTree.Item.page("Section Table", SectionTableView.class));
 
         return map;
     }
@@ -43,16 +47,22 @@ public class MainModel {
     // Constructing the Sidebar Tree
     private NavTree.Item createTree() {
         var general = NavTree.Item.group("General", new FontIcon(Material2OutlinedMZ.SPEED));
-        general.getChildren().setAll(NAV_TREE.get(ThemePage.class));
+        general.getChildren().setAll(  );
         general.setExpanded(true);
+
+        var spaceTime = NavTree.Item.group("SpaceTime", new FontIcon(Material2OutlinedMZ.PUBLIC));
+        spaceTime.getChildren().setAll(NAV_TREE.get(SpaceTimeFormView.class), NAV_TREE.get(SpaceTimeTableView.class));
+        spaceTime.setExpanded(true);
+
+        var sections = NavTree.Item.group("Sections", new FontIcon(Material2OutlinedMZ.UPGRADE));
+        sections.getChildren().setAll(NAV_TREE.get(SectionFormView.class), NAV_TREE.get(SectionTableView.class));
+        sections.setExpanded(true);
 
         var debug = NavTree.Item.group("DEBUG", new FontIcon(Material2OutlinedMZ.SETTINGS));
         debug.getChildren().setAll(NAV_TREE.get(Button.class));
 
-        NavTree.Item pageItem = NavTree.Item.page("Theme Settings", new FontIcon(Material2OutlinedMZ.MAIL), ThemePage.class);
-
         var root = NavTree.Item.root();
-        root.getChildren().setAll(debug, general, pageItem);
+        root.getChildren().setAll(debug, general, spaceTime, sections);
 
         return root;
     }
